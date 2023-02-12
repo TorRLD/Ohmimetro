67902482ed8103b14309b37ae97389ce69cdd56c
Heitor Lemos <102774638+TorRLD@users.noreply.github.com>
2023-02-12 18:59:28 -0300

    Update example.cpp

--- ohmimeter/src/example.cpp
+++ ohmimeter/src/example.cpp
@@ -1,6 +1,7 @@
 #include "ohmimeter.h"
 #include "Arduino.h"
 
+//Cria um objeto ohmi com o pino A3 recebendo o valor de tensão do divisor de tensão
 ohmi resistencia(A3);
 
 void setup()
@@ -12,6 +13,7 @@ void loop()
 {
   delay(500);
 
+  // O objeto utiliza o método resistance sendo que foi considerada uma fonte de 5 Volts e um resistor de 1kOhm no divisor de tensão
   float result = resistencia.resistance(5, 1000);
   Serial.println(result,4);
 
